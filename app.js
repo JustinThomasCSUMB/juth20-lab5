e978843c35e8d0f28557effab02722ce1435d7ab
justinthomascsumb <justhomas@csumb.edu>
2020-12-04 05:30:46 +0000

    ready for sql

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+// routes
+app.get("/", async function(req, res){
+    let apiUrl = 'https://api.unsplash.com/photos/random/?client_id=JfD7ECwC3AABlDFDCHbLPRN09od8meP3Qa1YxKjF-MI&featured=true&orientation=landscape';
+    let response = await fetch(apiUrl);
+    let data = await response.json();
+    
+    res.render("index", {"imageUrl":data.urls.small});
+});// index
+
+app.get("/search", async function(req, res){
+    let keyword = "";
+    if(req.query.keyword){
+        keyword = req.query.keyword;
+    }
+    
+    let apiUrl = `https://api.unsplash.com/photos/random/?count=9&client_id=JfD7ECwC3AABlDFDCHbLPRN09od8meP3Qa1YxKjF-MI&featured=true&orientation=landscape&query=${keyword}`;
+    let response = await fetch(apiUrl);
+    let data = await response.json();
+    
+    let imageUrlArray = [];
+    for(let i=0; i < data.length; i++){
+        imageUrlArray.push(data[i].urls.small);
+    }
+    
+    res.render("results", {"imageUrl":data[0].urls.small, "imageUrlArray":imageUrlArray});
+    
+});// search
+
+
+// starting server
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Express server is running...");
+});
---
+++ public/js/functions.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    
+    $(".favoriteIcon").on("click", function(){
+        if($(this).attr("src") == "img/favorite.png"){
+            $(this).attr("src", "img/favorite_on.png");
+        }
+        else {
+            $(this).attr("src", "img/favorite.png");    
+        }
+    })// favoriteIcon
+    
+});// ready
